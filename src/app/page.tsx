b678c35854c5da6e2502982dfea84db7b3fc7932
Jess <jessanderson.1145@gmail.com>
2025-09-07 13:11:12 -0400

    mobile friendly fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,13 +15,6 @@ const carouselSlides = [
     logo: '/graphics/nancys-logo.png',
     description: 'The Nancys are an indie rock band that started with a simple recipe: good friends, great music, and the occasional interruption from their beloved cats.',
     link: '/artists'
-  },
-  {
-    id: 'show-promo',
-    backgroundImage: '/media/elise-crowd2.webp',
-    logo: '/graphics/bts-logo.png',
-    description: 'Welcome back to school! Come see The Nancys and many other talented acts perform live at Heartwood Soundstage on August 23rd!',
-    link: 'https://www.heartwoodsoundstage.com/shows/back-to-school-fest-23-aug#tickets'
   }
 ];
 
